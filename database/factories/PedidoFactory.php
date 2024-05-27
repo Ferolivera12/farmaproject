9e1b882ce1fde9f9fbcc05465dea684c16ec8795
Ambar Rojas Gonzalez <11a10101z11@gmail.com>
2024-05-27 10:34:28 -0600

    Agregacion de factories

--- database/factories/PedidoFactory.php
+++ database/factories/PedidoFactory.php
@@ -2,15 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Models\Pedido;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Faker\Generator as Faker;
-use Illuminate\Support\Facades\App;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pedido>
  */
 class PedidoFactory extends Factory
 {
+    protected $model = Pedido::class;
+
     /**
      * Define the model's default state.
      *
@@ -18,8 +19,18 @@ class PedidoFactory extends Factory
      */
     public function definition(): array
     {
-        return [
+        $cantidad = $this->faker->numberBetween(1, 100);
+        $precioUnitario = $this->faker->randomFloat(2, 1, 100);
+        $importeTotal = $cantidad * $precioUnitario;
+        $estados = ['pendiente', 'recibido', 'cancelado'];
 
+        return [
+            'fecha_hora' => $this->faker->dateTime(),
+            'cantidad' => $cantidad,
+            'precio_unitario' => $precioUnitario,
+            'importe_total' => $importeTotal,
+            'estado' => $this->faker->randomElement($estados),
+            'id_usuario' => \App\Models\User::factory(), // Asume que existe un modelo de Usuario
         ];
     }
 }
